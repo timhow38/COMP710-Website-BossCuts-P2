b9ee819ca90275711574a8830d847f9482f48e76
Tinothy <timhow38@gmail.com>
2019-10-07 22:42:37 +1300

    Update
    07-10-19

--- Laravel Project/BossCuts/resources/views/auth/login.blade.php
+++ Laravel Project/BossCuts/resources/views/auth/login.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.auth')
 
 @section('content')
-<!-- BTN Trigger - Login Form-->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login">
-  Login
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">{{ ucfirst(config('app.name')) }} Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
                     @if (count($errors) > 0)
                         <div class="alert alert-danger">
                             <strong>Whoops!</strong> There were problems with input:
@@ -80,15 +64,4 @@
                             </div>
                         </div>
                     </form>
-
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        
-      </div>
-    </div>
-  </div>
-</div>
 @endsection
